b584f5e7c39848ac28bc9081413a2d84231981b3
GeekySazzad <71886049+sazzadalam112@users.noreply.github.com>
2023-12-09 22:57:56 +0530

    add social Links

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React from 'react'
 import Navbar from './components/Navbar'
 import Home from './components/Home'
+import SocialLinks from './components/SocialLinks'
 
 const App = () => {
   return (
     <div>
       <Navbar />
       <Home />
+
+      <SocialLinks />
     </div>
   )
 }
