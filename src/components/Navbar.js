85b0fc430c7465b483388469bdb84ce4e133f2d2
GeekySazzad <71886049+sazzadalam112@users.noreply.github.com>
2023-12-09 22:21:21 +0530

    add home

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,9 +1,10 @@
-import { list } from 'postcss';
-import React from 'react';
+// import { list } from 'postcss';
+import React, { useState } from 'react';
 import {FaBars,FaTimes} from "react-icons/fa"
 
 const Navbar = () => {
-
+   
+    const [nav , setNav] = useState(false);
     const links =[
         {
             id: 1,
@@ -28,7 +29,7 @@ const Navbar = () => {
             <h1 className='text-5xl font-light ml-2'>Sazzad</h1>
         </div>
 
-        <ul className='flex'>
+        <ul className='hidden md:flex'>
             {
                 links.map(({link, id}) => (
                       <li
@@ -38,6 +39,21 @@ const Navbar = () => {
             }
           
         </ul>
+        <div onClick={() => setNav(!nav)} className='cursor-pointer pr-4 z-10 text-gray-500 md:hidden'>
+            {nav ? <FaTimes size={30} /> : <FaBars size={30} />}
+        </div>
+
+        {nav && (
+
+            <ul className='flex flex-col justify-center items-center absolute top-0 left-0 w-full h-screen bg-gradient-to-b from-black to-gray-800 text-gray-500 text-gray-500'>
+        {links.map(({link, id}) => (
+                      <li
+                      key={id} className='p-4 cursor-pointer capitalize py-6 text-4xl'>{link}
+                      </li>
+                )) }   
+        </ul>
+        )}
+        
      
     </div>
   )
