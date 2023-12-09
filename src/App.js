1e316ed32bddac4703d48a4ee708e1411d3acfbc
GeekySazzad <71886049+sazzadalam112@users.noreply.github.com>
2023-12-09 23:09:16 +0530

    about

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import Navbar from './components/Navbar'
+import About from './components/About'
 import Home from './components/Home'
 import SocialLinks from './components/SocialLinks'
 
@@ -8,6 +9,7 @@ const App = () => {
     <div>
       <Navbar />
       <Home />
+      <About />
 
       <SocialLinks />
     </div>
---
+++ src/components/About.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <div name = "about" className='w-full bg-gradient-to-b from-gray-800 to-black text-white'>
+     
+     <div className='max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full'>
+         <div className='pb-8'>
+            <p className='text-4xl font-bold inline border-b-4 border-gray-500'>About</p>
+         </div>
+         <p className='text-xl mt-20'>
+         I am Md Sazzad Alam I hold a Bechelor of Science in Computere Science from Nalanda College of Engineering with a CGPA of 7.99. I am proficient in programming languages such as JavaScript, Python and HTML/CSS. I have also worked with various developer tools such as VS Code
+         </p>
+       
+     </div>
+    </div>
+  )
+}
+
+export default About
