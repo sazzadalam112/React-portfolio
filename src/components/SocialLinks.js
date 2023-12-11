ae2f5e610f5426d7434be0d0c24a04424b9540a3
GeekySazzad <71886049+sazzadalam112@users.noreply.github.com>
2023-12-11 18:29:39 +0530

    update twitter

--- src/components/SocialLinks.js
+++ src/components/SocialLinks.js
@@ -1,6 +1,6 @@
 import React from 'react'
-import {FaGithub,FaLinkedin,FaInstagram} from "react-icons/fa";
-import {HiOutlineMail} from "react-icons/hi";
+import {FaGithub,FaLinkedin,FaInstagram,FaTwitter} from "react-icons/fa";
+// import {HiOutlineMail} from "react-icons/hi";
 import {BsFillPersonLinesFill} from "react-icons/bs";
 
 
@@ -10,7 +10,7 @@ const SocialLinks = () => {
         id: 1, 
         child: (
             <>
-            LinkedIn <FaLinkedin  size={30}/>
+            <FaLinkedin  size={30}LinkedIn />
             </>
         ),
         href: "https://www.linkedin.com/in/md-sazzad-alam/",
@@ -20,7 +20,7 @@ const SocialLinks = () => {
         id: 2, 
         child: (
             <>
-            GitHub <FaGithub  size={30}/>
+             <FaGithub  size={30}GitHub/>
             </>
         ),
         href: "https://github.com/sazzadalam112",
@@ -30,7 +30,7 @@ const SocialLinks = () => {
         id: 3, 
         child: (
             <>
-            Instagram <FaInstagram  size={30}/>
+            <FaInstagram  size={30} Instagram/>
             </>
         ),
         href: "https://www.instagram.com/geekysazzad/",
@@ -40,10 +40,10 @@ const SocialLinks = () => {
         id: 4, 
         child: (
             <>
-            Mail <HiOutlineMail  size={30}/>
+            <FaTwitter  size={30} Mail/>
             </>
         ),
-        href: "mailto:mdsazzadalamcp@gmail.com",
+        href: "https://twitter.com/mdsazza41750849",
         style: 'rounded-tr-md'
         
     },
@@ -51,7 +51,7 @@ const SocialLinks = () => {
         id: 5, 
         child: (
             <>
-            Resume <BsFillPersonLinesFill  size={30}/>
+           <BsFillPersonLinesFill  size={30} Resume />
             </>
         ),
         href: "/resume.png",
@@ -60,11 +60,11 @@ const SocialLinks = () => {
     },
  ]
   return (
-    <div className=' hidden lg:flex flex-col top-[35%] left-0 fixed'>
+    <div className=' sm:hidden lg:flex flex-col top-[42%] right-0 fixed'>
       <ul>
 
         {links.map(({id,child,href,style,download}) => (
-             <li key={id} className={'flex justify-between items-center w-40 h-14  px-4 ml-[-100px] hover:ml-[-10px] hover:rounded-md duration-300 bg-gray-500' + " " + style}>
+             <li key={id} className={'flex justify-between items-center w-40 h-12  px-4 mr-[-100px] hover:ml-[-10px] hover:rounded-md duration-300 bg-gray-500' + " " + style}>
             <a 
             href={href}
             className='flex justify-between items-center w-full text-white 
