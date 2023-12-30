33e789ccd6637659a71a3d017da7e963c04bcc56
GeekySazzad <71886049+sazzadalam112@users.noreply.github.com>
2023-12-31 02:46:33 +0530

    add new project

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -5,6 +5,7 @@ import myTravel from "../assets/mytravel.png";
 import myAnimation from "../assets/myanimation.png";
 import myShoppingList from "../assets/shoppingList.png"
 import myClock from "../assets/clock.png"
+import mytasty from "../assets/mytasty.png"
 
 const Portfolio = () => {
     const profile = [
@@ -14,11 +15,11 @@ const Portfolio = () => {
             demoLink: 'https://youtube-clone-iota-mauve.vercel.app/',
             codeLink: 'https://github.com/sazzadalam112/youtube-clone',
         },
-     
+
         {
             id: 2,
-            src: mySweggy,
-            demoLink: 'https://food-villa-coral.vercel.app/',
+            src: mytasty,
+            demoLink: 'https://tasty-yummies.vercel.app/',
             codeLink: 'https://github.com/sazzadalam112/Food-villa',
         },
         {
@@ -39,62 +40,63 @@ const Portfolio = () => {
             demoLink: 'https://shoppinglistitem.vercel.app/',
             codeLink: 'https://github.com/sazzadalam112/shoppinglistitem',
         },
-       
+
         {
             id: 6,
             src: myClock,
             demoLink: 'https://animated-clock-gilt.vercel.app/',
             codeLink: 'https://github.com/sazzadalam112/Animated-clock',
-            
+
         },
 
+
     ]
-  return (
-   <div 
-   name='Portfolio'
-   className=' bg-gradient-to-b from-black to-gray-800 w-full text-white md:h-screen'>
-    <div className='max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full'>
-        <div className='pb-8'>
-            <p className='text-4xl font-bold inline border-b-4'>Portfolio</p>
-            <p className='py-6'>Check out some my work right here</p>
-             </div>
-
-        <div className='grid sm:grid-cols-2 md:grid-cols-3 gap-8 px-12 sm:px-0'>
-            {
-              profile.map(({id,src,demoLink,codeLink}) => (
-              <div key={id} className='shadow-sm shadow-amber-900 rounded-lg'>
-                <img 
-                src={src} 
-                alt=''
-                className='rounded-md duration-200 hover:scale-105' 
-                />
-                <div className='flex items-center justify-center '>
-                <a
-                  href={demoLink}
-                  target='_blank'
-                  rel='noopener noreferrer'
-                  className='w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105 bg-gradient-to-t from-red-500 to to-blue-600 rounded-xl'>
-                  Demo
-                </a>
-                <a
-                  href={codeLink}
-                  target='_blank'
-                  rel='noopener noreferrer'
-                  className='w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105 bg-gradient-to-t from-red-500 to to-blue-600 rounded-xl'>
-                  Code
-                </a>
+    return (
+        <div
+            name='Portfolio'
+            className=' bg-gradient-to-b from-black to-gray-800 w-full text-white md:h-screen'>
+            <div className='max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full'>
+                <div className='pb-8'>
+                    <p className='text-4xl font-bold inline border-b-4'>Portfolio</p>
+                    <p className='py-6'>Check out some my work right here</p>
                 </div>
+
+                <div className='grid sm:grid-cols-2 md:grid-cols-3 gap-8 px-12 sm:px-0'>
+                    {
+                        profile.map(({ id, src, demoLink, codeLink }) => (
+                            <div key={id} className='shadow-sm shadow-amber-900 rounded-lg'>
+                                <img
+                                    src={src}
+                                    alt=''
+                                    className='rounded-md duration-200 hover:scale-105'
+                                />
+                                <div className='flex items-center justify-center '>
+                                    <a
+                                        href={demoLink}
+                                        target='_blank'
+                                        rel='noopener noreferrer'
+                                        className='w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105 bg-gradient-to-t from-red-500 to to-blue-600 rounded-xl'>
+                                        Demo
+                                    </a>
+                                    <a
+                                        href={codeLink}
+                                        target='_blank'
+                                        rel='noopener noreferrer'
+                                        className='w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105 bg-gradient-to-t from-red-500 to to-blue-600 rounded-xl'>
+                                        Code
+                                    </a>
+                                </div>
+                            </div>
+                        ))
+                    }
+
+                </div>
+
+
             </div>
-              ))  
-            }
-           
+
         </div>
-
-    
-    </div>
-
-   </div>
-  )
+    )
 }
 
 export default Portfolio;
